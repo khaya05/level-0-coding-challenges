5e471fa56138fb3bcca7b89efd2176eff2189f07
khaya05 <khayamnyandu5@gmail.com>
2022-04-11 13:52:17 +0200

    named var commonLetters as a constant

--- task_0.10.js
+++ task_0.10.js
@@ -1,5 +1,5 @@
 function getCommonChars(str1, str2) {
-    let commonLetters = [];
+    const commonLetters = [];
     newStr2 = str2.split('');
     for (let letter of newStr2) {
         if (str1.includes(letter)) {
