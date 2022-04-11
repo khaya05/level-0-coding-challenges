d637b92e9029befb58775b24182624270fbf1cc1
khaya05 <khayamnyandu5@gmail.com>
2022-04-11 13:21:36 +0200

    removed unnecessary code

--- task_0.5.js
+++ task_0.5.js
@@ -1,5 +1,5 @@
 function calcArea(a, b, c) {
-    let semiperimeter = s = 0.5 * (a + b + c);
+    let s = 0.5 * (a + b + c);  
     const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
     return area;
 };
