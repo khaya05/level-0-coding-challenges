76a64a6480a3f84ef729368b12d654b69c8b01cd
khaya05 <khayamnyandu5@gmail.com>
2022-04-13 12:27:45 +0200

    fixed few bugs, also added a few function calls

--- task_0.8.js
+++ task_0.8.js
@@ -1,6 +1,6 @@
  function formatTime(number) {
-    if (number < 60) {
-        return number + ` ${number > 1 ? 'minutes' : 'minute'}`
+    if (number < 60 ) {
+        return number + `${number !== 1 ? ' minutes' : ' minute'}`
     }
     else if (number === 60) {
         return '1 hour'
@@ -8,8 +8,15 @@
     else if (number > 60) {
         const numberOfHrs = Math.floor(number / 60);
         const minutes = number % 60;
-        const timeInFull = numberOfHrs + ` hour${numberOfHrs > 1 ? 's' : ''}, ` + minutes + ` minute${minutes > 1 ? 's' : ''}`;
+        const timeInFull = numberOfHrs + ` hour${numberOfHrs > 1 ? 's' : ''}, ` + minutes + ` minute${minutes !== 1 ? 's' : ''}`;
         return timeInFull;
     }
         
 };
+
+console.log(formatTime(0));
+console.log(formatTime(1));
+console.log(formatTime(2));
+console.log(formatTime(120));
+console.log(formatTime(121));
+console.log(formatTime(122));
